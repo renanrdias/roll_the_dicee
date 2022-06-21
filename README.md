8b140417c8497571bf63bd2fb5527219ad73e653
renan dias <renanrdias@gmail.com>
2022-06-21 12:01:27 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# dice_project
+
+## The main goal
+
+The main goal of the project is to apply core programming concepts. This app will provide some basic functionality by using setState() inside Stateful Widgets.
+
+## The App
+
+This app will simulate two dices that when pressed, they show us random number from 1 to 6 in their visible faces.
+
+## The main topics learned
+* Use Stateless Widgets to build the UI.
+* Use Stateful Widgets in order to update the UI.
+* Change properties of Widgets
+* Understand how the event onPressed works when a button is pressed.
+* Understand how setState works to mark the widget tree dirty, and therefore updates the UI on the next render.
+* Check out the Expanded Widget to build UI that adapts in certain way to the screen size of the user's device.
+* Use string interpolation.
+* Use basic concepts in Dart such as data types and functions.
+* Code and use Gesture Controls.
+
+## License
+
+This app is part of The App Brewery's Complete Flutter Development Bootcamp. You can check out the full course at [www.appbrewery.co](https://www.appbrewery.co)
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.deepOrange,
+      appBar: AppBar(
+        title: Text(
+          'Dicee!',
+        ),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: DicePage(),
+    ),
+  ));
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void updateNumbers() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: updateNumbers,
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: updateNumbers,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
